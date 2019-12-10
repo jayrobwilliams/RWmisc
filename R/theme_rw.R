6d3df8a792c0282c960afac5dd220812cf7d6641
Rob Williams <jayrobwilliams@gmail.com>
2019-12-10 12:10:39 -0600

    update to 0.0.2 and remove all Bayesian postestimation functions as they are now in BayesPostEst

--- R/theme_rw.R
+++ R/theme_rw.R
@@ -2,15 +2,16 @@
 #'
 #' A ggplot theme with no grid elements or gray background.
 #'
+#' @importFrom ggplot2 theme theme_bw element_blank
+#'
 #' @export
 #'
 #' @examples
-#' ggplot(mtcars, aes(x = hp, y = mpg)) +
-#' geom_point() +
+#' ggplot2::ggplot(mtcars, ggplot2::aes(x = hp, y = mpg)) +
+#' ggplot2::geom_point() +
 #' theme_rw()
 theme_rw <- function() {
   theme_bw() +
   theme(plot.background = element_blank(), panel.grid.minor = element_blank(),
         panel.grid.major = element_blank(), panel.border = element_blank())
-
 }
