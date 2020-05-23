ef90d1f4d9d0f8ba3a4725bcb7ffa3cdb59dc246
Rob Williams <jayrobwilliams@gmail.com>
2020-05-23 11:46:44 -0400

    add codecov

---
+++ tests/testthat/test-pointpolydist.R
@@ -0,0 +1,6 @@
+test_that("point.poly.dist works", {
+  polys <- st_sfc(st_polygon(list(rbind(c(0,0), c(0,1), c(1,1), c(1,0), c(0,0)))),
+                  crs = 4326)
+  points <- st_sfc(st_multipoint(rbind(c(.25, .5), c(.75, .5))), crs = 4326)
+  expect_equal(point.poly.dist(points, polys), 0.9013878, tolerance = 1e-5)
+})
