931bfd9a8cc43814825341d2e549ec1c5b2f48b0
Rob Williams <jayrobwilliams@gmail.com>
2020-05-24 12:05:11 -0400

    drop unnecessary condition in point.poly.dist

--- R/pointpolydist.R
+++ R/pointpolydist.R
@@ -26,12 +26,9 @@ point.poly.dist <- function(poly, point, max = T) {
   ## project point using polygon CRS
   point <- st_transform(point, st_crs(poly))
 
-  ## check to ensure polygon and point have same CRS
-  if (st_crs(poly) != st_crs(point)) stop('polygon and point do not share same CRS')
-
   ## extract border vertices and point coordinates
   border <- st_coordinates(poly)[, 1:2]
-  point <- st_coordinates(point)
+  point <- st_coordinates(point)[, 1:2]
 
   ## extract eastings and northings of all border vertices
   longs <- border[, 1]
--- tests/testthat/test-pointpolydist.R
+++ tests/testthat/test-pointpolydist.R
@@ -1,13 +1,11 @@
+poly_t <- st_sfc(st_polygon(list(rbind(c(0,0), c(0,1), c(1,1), c(1,0), c(0,0)))),
+                 crs = 4326)
+points_t <- st_sfc(st_multipoint(rbind(c(.25, .5), c(.75, .5))), crs = 4326)
+
 test_that("point.poly.dist works", {
-  polys <- st_sfc(st_polygon(list(rbind(c(0,0), c(0,1), c(1,1), c(1,0), c(0,0)))),
-                  crs = 4326)
-  points <- st_sfc(st_multipoint(rbind(c(.25, .5), c(.75, .5))), crs = 4326)
-  expect_equal(point.poly.dist(points, polys), 0.9013878, tolerance = 1e-5)
+  expect_equal(point.poly.dist(points_t, poly_t), 0.9013878, tolerance = 1e-5)
 })
 
 test_that("point.poly.dist works with arguments", {
-  polys <- st_sfc(st_polygon(list(rbind(c(0,0), c(0,1), c(1,1), c(1,0), c(0,0)))),
-                  crs = 4326)
-  points <- st_sfc(st_multipoint(rbind(c(.25, .5), c(.75, .5))), crs = 4326)
-  expect_equal(point.poly.dist(points, polys, max = FALSE), 0.559017, tolerance = 1e-5)
+  expect_equal(point.poly.dist(points_t, poly_t, max = FALSE), 0.559017, tolerance = 1e-5)
 })
