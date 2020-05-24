6d144573019368e279f446469daa5b35e6e9f0ad
Rob Williams <jayrobwilliams@gmail.com>
2020-05-24 11:33:35 -0400

    fix error in projectUTM for points and simplify code for sf (from sp)

--- R/projectUTM.R
+++ R/projectUTM.R
@@ -17,27 +17,11 @@
 
 projectUTM <- function(sfo) {
 
-  ## determine if spatial object is a SpatialPoints or SpatialPolygons object and use either
-  ## the coords or polygons slot to access the longitude(s) of the spatial object
-  if (attr(st_geometry(sfo), 'class')[1] == 'sfc_POINT') {
+  ## find average UTM zone using longitude(s) of sf object
+  zone <- chooseUTM(mean(st_coordinates(sfo)[, 1]))
 
-    ## find average UTM zone using longitude(s) of SpatialPoints object
-    zone <- chooseUTM(st_coordinates(sfo)[1])
-
-    ## save latitude mean to determine if features falls in southern hemisphere
-    lat.mean <- mean(st_coordinates(sfo)[2])
-
-  }
-
-  if (attr(st_geometry(sfo), 'class')[1] == 'sfc_MULTIPOLYGON') {
-
-    ## find average UTM zone using longitude(s) of SpatialPolygons object
-    zone <- chooseUTM(mean(st_coordinates(sfo)[, 1]))
-
-    ## save latitude mean to determine if features falls in southern hemisphere
-    lat.mean <- mean(st_coordinates(sfo)[, 2])
-
-  }
+  ## save latitude mean to determine if majority of features fall in southern hemisphere
+  lat.mean <- mean(st_coordinates(sfo)[, 2])
 
   ## if average of latitude values is negative, add +south the coordinate reference system
   if (lat.mean >= 0) {
--- tests/testthat/test-projectUTM.R
+++ tests/testthat/test-projectUTM.R
@@ -1,5 +1,31 @@
-test_that("projectUTM works", {
-  library(sf)
-  nc <- st_read(system.file("shape/nc.shp", package="sf"))
+library(sf)
+nc <- st_read(system.file("shape/nc.shp", package="sf"))
+poly_t <- st_sfc(st_polygon(list(rbind(c(10,-10), c(10,-20), c(20,-20),
+                                       c(20,-10), c(10,-10)))),
+                 crs = 4326)
+
+test_that("projectUTM works with sfc_POLYGON", {
+  expect_equal(st_crs(projectUTM(st_cast(nc[1,], "POLYGON")))[["input"]],
+               "+proj=utm +zone=17")
+})
+
+test_that("projectUTM works with sfc_MULTIPOLYGON", {
   expect_equal(st_crs(projectUTM(nc))[["input"]], "+proj=utm +zone=17")
+})
+
+
+test_that("projectUTM works with sfc_POINT", {
+  expect_equal(st_crs(projectUTM(st_centroid(nc)))[["input"]], "+proj=utm +zone=17")
+})
+
+test_that("projectUTM works with sfc_POINT", {
+  expect_equal(st_crs(projectUTM(st_cast(st_centroid(nc), 'MULTIPOINT')))[["input"]], "+proj=utm +zone=17")
+})
+
+test_that("projectUTM works with southern latitude", {
+  expect_equal(st_crs(projectUTM(poly_t))[["input"]], "+proj=utm +south +zone=33")
+})
+
+test_that("projectUTM works with sf and sfc", {
+  expect_equal(st_crs(projectUTM(st_geometry(nc))), st_crs(projectUTM(nc)))
 })
