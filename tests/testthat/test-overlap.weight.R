ee329a094c50780c25433ce54b5691714ea8b3f3
Rob Williams <jayrobwilliams@gmail.com>
2021-06-19 20:52:25 -0400

    silence warnings on tests

--- tests/testthat/test-overlap.weight.R
+++ tests/testthat/test-overlap.weight.R
@@ -8,29 +8,20 @@ raster_t <- raster(nrows = 10, ncols = 10, xmn = 0, xmx = 10, ymn = 0, ymx = 10,
                    vals = 1:100, crs = CRS(st_crs(polys_t)$proj4string))
 
 test_that("overlap.weight works with sfc_POLYGON", {
-  expect_equal(overlap.weight(raster_t, polys_t)[4,4], 28)
+  expect_equal(overlap.weight(raster_t, polys_t, warn = FALSE)[4,4], 28)
 })
 
 test_that("overlap.weight works with sf", {
-  expect_equal(overlap.weight(raster_t,
-                              st_sf(data.frame(a = 1:2,
-                                               geometry = polys_t)))[4,4], 28)
-})
-
-test_that("overlap.weight works with SpatialPolygons", {
-  expect_equal(overlap.weight(raster_t, as(polys_t, 'Spatial'))[4,4], 28)
-})
-
-test_that("overlap.weight works with SpatialPolygonsDataFrame", {
-  expect_equal(overlap.weight(raster_t,
-                              as(st_sf(data.frame(a = 1:2, geometry = polys_t)),
-                                 'Spatial'))[4,4], 28)
+  expect_equal(overlap.weight(raster_t, st_sf(data.frame(a = 1:2,
+                                                         geometry = polys_t)),
+                              warn = FALSE)[4,4], 28)
 })
 
 test_that("overlap.weight works with count", {
-  expect_equal(overlap.weight(raster_t, polys_t, count = TRUE)[4,4], 2)
+  expect_equal(overlap.weight(raster_t, polys_t, count = TRUE,
+                              warn = FALSE)[4,4], 2)
 })
 
-test_that("overlap.weight works silently", {
-  expect_silent(overlap.weight(raster_t, polys_t, warn = FALSE))
+test_that("overlap.weight warns about different extents", {
+  expect_warning(overlap.weight(raster_t, polys_t, warn = TRUE))
 })
