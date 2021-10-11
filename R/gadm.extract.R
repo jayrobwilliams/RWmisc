baf5007f88ebf75c71ca24a44792c9cfb79f511a
Rob Williams <jayrobwilliams@gmail.com>
2021-10-11 12:32:09 -0400

    finish documenting gadm.extract

--- R/gadm.extract.R
+++ R/gadm.extract.R
@@ -1,25 +1,41 @@
 #' Extract layers by country from GADM GeoPackage file
 #'
-#' Extract one or more levels of administrative unit geometries from
+#' Extract one or more levels of administrative unit geometries from the GADM
+#' database in GeoPackage format
 #'
 #' @param input GeoPackage file to read from
 #' @param output name of file to save output to
 #' @param countries country or countries to limit results to, if `NULL` returns
 #' all countries
-#' @param level level(s) of administrative units 0:4 to extract; note level 4 is
-#' not available for all countries
+#' @param level level(s) of administrative units 0:5 to extract; note not all
+#' levels are defined for all countries
 #' @param ... additional arguments passed to [sf::st_write()]
+#'
+#' @details This function is designed to extract subsets of the
+#' [Database of Global Administrative Areas (GADM)](https://gadm.org/). It uses
+#' the version of the database in GeoPackage format that provides one layer for
+#' each level of administrative division, available at
+#' [https://gadm.org/download_world.html](https://gadm.org/download_world.html).
+#' The current version of this file is `gadm36_levels.gpkg`. It is intended
+#' for programmatic and reproducible subsetting of the database without
+#' requiring the user to individually download specific country data files.
 #'
 #' @export
 #'
 #' @examples
 #' \dontrun{
-#' gadm.extract("gadm34_levels.gpkg", "Nordics.gpkg",
+#' ## extract
+#' gadm.extract("gadm36_levels.gpkg", "Nordics.gpkg",
 #'              c("Denmark", "Finland", "Iceland", "Norway", "Sweden"),
-#'              level = 0:4)
+#'              level = 0:2)
+#'
+#' ## add layers 3 and 4, use delete_layer = TRUE to rewrite existing layers
+#' gadm.extract("gadm36_levels.gpkg", "Nordics.gpkg",
+#'              c("Denmark", "Finland", "Iceland", "Norway", "Sweden"),
+#'              level = 0:4, delete_layer = TRUE)
 #' }
 
-gadm.extract <- function(input, output, countries = NULL, level = 0:4, ...) {
+gadm.extract <- function(input, output, countries = NULL, level = 0:5, ...) {
 
   ## add file extension to output if missing
   if (!grepl('^.*\\.gpkg$', output)) output <- paste0(output, '.gpkg')
